b80e85288b8765ed56909598a357487a988e5caa
Aaron Berns <anberns@gmail.com>
2019-02-24 12:23:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+function findMatching(arr, str) {
+  matches = [];
+  lowerStr = str.toLowerCase();
+  for (const i of arr) {
+    lowerDriver = i.toLowerCase();
+    if (lowerDriver === lowerStr){
+      matches.push(i);
+    }
+  }
+  return matches;
+}
+
+function fuzzyMatch(arr, str) {
+  return arr.filter( function (i) { return i.slice(0, str.length) === str});
+}
+
+function matchName(obj, str) {
+  return obj.filter( function (i) { return i.name.toLowerCase() === str.toLowerCase()});
+}
