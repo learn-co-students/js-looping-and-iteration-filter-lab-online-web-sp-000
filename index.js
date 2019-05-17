028bf7fc204f3966a605e6d39cb8fe648e64f3c3
jonmackeyparty <jon@jonmackey.party>
2019-05-17 13:35:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+
+function findMatching(array, name) {
+  return array.filter(function(key) {return key.toLowerCase() === name.toLowerCase();});
+}
+
+function fuzzyMatch(array, name) {
+  return array.filter(function(key) {return key.startsWith(name);});
+}
+
+function matchName(array, name) {
+  return array.filter(function(key) {return key.name.toLowerCase() === name.toLowerCase();});
+}
