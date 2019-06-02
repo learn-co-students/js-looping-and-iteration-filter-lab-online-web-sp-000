89c950ec1a09a8bc07a29d0fa755acfe7112abdf
Btate712 <btate712@gmail.com>
2019-06-02 09:36:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+function findMatching(drivers, searchString) {
+  searchString = searchString.toLowerCase();
+  return drivers.filter(function (driver) { return searchString === driver.toLowerCase();});
+}
+
+function fuzzyMatch(drivers, searchString) {
+  searchString = searchString.toLowerCase();
+  return drivers.filter(function (driver) { return driver.toLowerCase().startsWith(searchString);});
+}
+
+function matchName(drivers, searchString) {
+  return drivers.filter(function (driver) { return driver.name === searchString; });
+}
