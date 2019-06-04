b6375190cecdd2e763efb9b0a6f825cd26301b64
alexmpalermo <alexmpalermo@gmail.com>
2019-06-03 20:32:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  const thisArray = drivers.filter(function (driv) { return driv.toUpperCase() === name.toUpperCase(); });
+  return thisArray
+}
+
+function fuzzyMatch(drivers, letters) {
+  letters = letters.toUpperCase();
+  return drivers.filter(function (driv) { return driv.toUpperCase().startsWith(letters);});
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function (driv) { return driv.name === name; });
+}
