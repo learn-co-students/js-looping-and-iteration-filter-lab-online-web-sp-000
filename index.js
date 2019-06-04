eaae33c53b5a487c76f4c7664ceace7d6edf343a
Sharon Alajajian <sharonoptsout@gmail.com>
2019-06-04 10:37:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter(function (f) { return f.toLowerCase() == string.toLowerCase(); });
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function (f) { return f.startsWith(string); });
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function (f) { return f.name.toLowerCase() == string.toLowerCase(); });
+}
