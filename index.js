2d2422622aa9977ae2e48861cfed54787579a24f
Diana Badas <diana.badas@gmail.com>
2019-07-28 13:08:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(list, name) {
+  return list.filter(function(driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(list, initial) {
+  return list.filter(function(driverName) {
+    return driverName.slice(0, initial.length) === initial;
+  });
+}
+
+function matchName(list, name) {
+  return list.filter(function(driver) {
+    return driver.name.toLowerCase() === name.toLowerCase;
+  });
+}
