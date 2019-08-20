97711a032cc068127650ed7e8fccf6ad96350fd4
Rachel <williamsrachelada@gmail.com>
2019-08-20 13:10:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function (element) { return name.toLowerCase() === element.toLowerCase(); });
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function (el) { return el.startsWith(string); });
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function (el) { return el.name === string; });
+}
