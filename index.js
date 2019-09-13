35a353bab61cc627722665ccaef960f353298c9e
github username <github email address>
2019-09-13 11:33:30 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+const findMatching = (drivers, name) => {
+    return drivers.filter(driver => {
+        return driver.toLowerCase() === name.toLowerCase();
+    });
+};
+
+const fuzzyMatch = (drivers, partOfName) => {
+    let lengthOfName = partOfName.length;
+    return drivers.filter(driver => {
+        return driver.slice(0, lengthOfName) === partOfName;
+    } );
+};
+
+const matchName = (drivers, name) => {
+    return drivers.filter(driver => {
+        return driver.name.toLowerCase() === name.toLowerCase();
+    });
+};
