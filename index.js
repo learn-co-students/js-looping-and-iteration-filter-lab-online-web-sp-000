e2b17d120035060d5c8896dbbfe006e00669b993
Tsunamins <reillyamr@gmail.com>
2019-09-23 11:56:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+function findMatching(drivers, name){
+   return drivers.filter(n => n.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(function(n){
+        return letters.toLowerCase() === n.slice(0, letters.length).toLowerCase();
+    })
+}
+
+function matchName(drivers, string){
+    return drivers.filter(o => o.name === string)
+
+}
