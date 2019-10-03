7f6616d79092c1332c9131c53b2590b1c540df48
Ramon Chavez <rachavez1974@gmail.com>
2019-10-03 19:25:37 -0400

    Finish js-looping-and-iteration-filter-lab-online-web-sp-000

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file
+
+function findMatching(drivers, name){
+  const drivers_list = [];
+    for(const driver of drivers){
+      if (driver.toLowerCase() === name.toLowerCase()){
+        drivers_list.push(driver);
+      }
+    }
+  return drivers_list;
+}
+
+function fuzzyMatch(drivers, chars){
+  const drivers_list = [];
+  for(const driver of drivers){
+      if (driver.startsWith(chars)){
+        drivers_list.push(driver);
+      }
+    }
+  return drivers_list;
+
+}
+
+function matchName(drivers, name){
+  const drivers_list = [];
+
+  for (const driver of drivers) {
+    if(driver.name === name){
+      drivers_list.push(driver);
+    } 
+  }
+  return drivers_list;
+}
